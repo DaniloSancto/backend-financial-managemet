2c6d13608a9cfb79b26b9b637489b0d364785f46
Danilo Santos <danilo.d.santos04@gmail.com>
2026-01-30 00:42:21 -0300

    first commit

---
+++ src/main/java/dev/danilosantos/financialmanagement/FinancialmanagementApplication.java
@@ -0,0 +1,13 @@
+package dev.danilosantos.financialmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialmanagementApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FinancialmanagementApplication.class, args);
+	}
+
+}
